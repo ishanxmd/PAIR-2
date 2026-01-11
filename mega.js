1e980b54c0ef14bc869d19ceb0a95fec7a9fe5a5
ishanxmd <ishanmadusanke548@gmail.com>
2026-01-11 14:16:08 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
     email: 'ishanmadusanke338@gmail.com',
-    password: 'da7cLQ2-dnz:6NY',
+    password: 'qH:T_UYhH3m4fM_',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
